53575756684376302b0679d0d3aa53bd439a510b
Christina Thee Roperto <christinatheeroperto@catalyst-au.net>
2021-08-19 15:14:04 +1000

    implementing restore system

---
+++ backup/moodle2/restore_local_quiz_summary_option_plugin.class.php
@@ -0,0 +1,52 @@
+
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Lib functions.
+ *
+ * @package   local_quiz_summary_option
+ * @author    Christina Roperto (christinatheeroperto@catalyst-au.net)
+ * @copyright Catalyst IT
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// This activity has not particular settings but the inherited from the generic
+// backup_activity_task so here there isn't any class definition, like the ones
+// existing in /backup/moodle2/backup_settingslib.php (activities section)
+
+/**
+ * Define all the restore steps that will be used by the backup_local_quiz_summary_option_plugin
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+class restore_local_quiz_summary_option_plugin extends restore_local_plugin {
+    protected function define_module_plugin_structure() {
+        $paths = array();
+
+        // This will call a function that starts with process_{name}. In this case it's calling process_quiz_summary function
+        $paths[] = new restore_path_element('quiz_summary', $this->get_pathfor(''));
+        return $paths;
+    }
+
+    public function process_quiz_summary($data) {
+        global $DB;
+
+        $data['cmid'] = $this->task->get_moduleid();
+        $DB->insert_record('local_quiz_summary_option', $data, false);
+    }
+}
